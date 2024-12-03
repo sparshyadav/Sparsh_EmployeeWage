e887b8cd325ee3023f56fd47d6006e2e39827f81
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-12-03 20:57:57 +0530

    [Sparsh] Refactor: Total Wage Calculate based on type of Work

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,13 +1,13 @@
-// UC1 - Ability to check Employee is Present or Absent
-const randomNumber = Math.random().toFixed(2);
-let attendence = 0;
-if (randomNumber > 0.5) {
-    attendence = 1;
-    console.log("The Employee is Present");
-}
-else {
-    console.log("The Employee is Absent");
-}
+// // UC1 - Ability to check Employee is Present or Absent
+// const randomNumber = Math.random().toFixed(2);
+// let attendence = 0;
+// if (randomNumber > 0.5) {
+//     attendence = 1;
+//     console.log("The Employee is Present");
+// }
+// else {
+//     console.log("The Employee is Absent");
+// }
 
 // // UC2 - Ability to Calculate Employee Wage based on Part time or Full Time work
 // let workType = 0;
@@ -29,36 +29,74 @@ else {
 //         console.log(`Due to No Working Hours, the total Wage is $0`);
 // }
 
-// UC3 - Refactor the Code to write a function to get work hours
-function calculateWages() {
+// // UC3 - Refactor the Code to write a function to get work hours
+// function calculateWages() {
+//     let workType = 0;
+//     if (randomNumber < 0.33) {
+//         workType = 1;
+//     }
+//     else if (randomNumber > 0.33 && randomNumber < 0.66) {
+//         workType = 2;
+//     }
+
+//     let wage = 0;
+//     switch (workType) {
+//         case 1:
+//             console.log(`The Employee Wage for 8 Hours on $20/hour is: ${20 * 8}`);
+//             wage = 20 * 8;
+//             break;
+//         case 0:
+//             console.log(`The Employee Wage for 4 Hours on $20/hour is: ${20 * 4}`);
+//             wage = 20 * 4;
+//             break;
+//         case 2:
+//             console.log(`Due to No Working Hours, the total Wage is $0`);
+//             break;
+//     }
+
+//     return wage;
+// }
+// calculateWages();
+
+// // UC4 - Calculating Wages for a Month
+// let oneDayWage = calculateWages();
+// let totalWage = 20 * oneDayWage;
+// console.log(`The Total Wage of Employee for a Month is $${totalWage}`);
+
+// UC5 - Calculate Wages till total working hours of 160 or max days of 20 is reached
+let totalHours = 0;
+let totalDays = 0;
+let totalWage = 0;
+
+while (totalHours < 160 && totalDays < 20) {
+    let randomNumber = Math.random().toFixed(2);
     let workType = 0;
+
     if (randomNumber < 0.33) {
         workType = 1;
-    }
-    else if (randomNumber > 0.33 && randomNumber < 0.66) {
+    } else if (randomNumber < 0.66) {
+        workType = 0;
+    } else {
         workType = 2;
     }
 
-    let wage = 0;
     switch (workType) {
         case 1:
-            console.log(`The Employee Wage for 8 Hours on $20/hour is: ${20 * 8}`);
-            wage = 20 * 8;
+            totalHours += 8;
+            totalDays++;
+            totalWage += 20 * 8;
             break;
         case 0:
-            console.log(`The Employee Wage for 4 Hours on $20/hour is: ${20 * 4}`);
-            wage = 20 * 4;
+            totalHours += 4;
+            totalDays++;
+            totalWage += 20 * 4;
             break;
         case 2:
-            console.log(`Due to No Working Hours, the total Wage is $0`);
+            totalDays++;
             break;
     }
-
-    return wage;
 }
-// calculateWages();
 
-// UC4 - Calculating Wages for a Month
-let oneDayWage = calculateWages();
-let totalWage = 20 * oneDayWage;
-console.log(`The Total Wage of Employee for a Month is $${totalWage}`);
+console.log(`Total Working Hours: ${totalHours}`);
+console.log(`Total Working Days: ${totalDays}`);
+console.log(`Total Wage for the Month: $${totalWage}`);
